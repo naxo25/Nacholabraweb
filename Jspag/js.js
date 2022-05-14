9c996f4d399a3168826d9e922f6eaa5128127f69
naxo25 <y.labra.25@gmail.com>
2022-05-13 21:27:41 -0400

    cargando scripts de forma diferida

--- Jspag/js.js
+++ Jspag/js.js
@@ -1,33 +1,47 @@
-const {createApp} = Vue;
-const application = createApp({
-  data() {
-    return {
-      desc_presentacion,
-      header,
-      icons,
-      load: false,
-      loadSection: {},
-      noimg: 'https://www.cronj.com/blog/wp-content/uploads/React-Hook.png'
-    }
-  },
-  computed:{
-    Portafolio() {
-      let arr = portafolio.slice(0,6);
-      return arr;
-    }
-  },
-  mounted () {
-    window.addEventListener('scroll', () => {
-      const a = document.querySelector('header');
-      a.classList.toggle('sticky', window.scrollY > 0);
-      if (scrollY > 0 && !this.loadSection[0]) this.loadSection[0] = true;
-      if (scrollY > 150 && !this.loadSection[1]) this.loadSection[1] = true;
-      if (scrollY > 500 && !this.loadSection[2]) this.loadSection[2] = true;
+  let load = true;
+  const script = document.createElement('script');
+
+  script.onload = function() {
+    console.log("Vue Script loaded and ready");
+
+    document.querySelector('.loader').style.display = 'none';
+    const {createApp} = Vue;
+    const application = createApp({
+      data() {
+        return {
+          desc_presentacion,
+          header,
+          icons,
+          load,
+          loadSection: {},
+          noimg: 'https://www.cronj.com/blog/wp-content/uploads/React-Hook.png'
+        }
+      },
+      computed:{
+        Portafolio() {
+          let arr = portafolio.slice(0,6);
+          return arr;
+        }
+      },
+      mounted () {
+        window.addEventListener('scroll', () => {
+          const a = document.querySelector('header');
+          a.classList.toggle('sticky', window.scrollY > 0);
+          if (scrollY > 0 && !this.loadSection[0]) this.loadSection[0] = true;
+          if (scrollY > 150 && !this.loadSection[1]) this.loadSection[1] = true;
+          if (scrollY > 500 && !this.loadSection[2]) this.loadSection[2] = true;
+        });
+      }
     });
-    document.addEventListener("DOMContentLoaded", () => {
-      this.load = true;
-      console.log('DOMContentLoaded ready');
-    })
-  }
-});
-application.mount('#app')
+    application.mount('#app');
+  };
+
+script.src = 'https://unpkg.com/vue/dist/vue.global.prod.js';
+document.getElementsByTagName('head')[0].appendChild(script);
+
+const toggleMenu = () => {
+  const a = document.querySelector('.menuToggle');
+  const b = document.querySelector('.navigation');
+  a.classList.toggle('active');
+  b.classList.toggle('active');
+}
